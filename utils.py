d5c696da7827205a259fd676785285a2b307acbd
p1x31 <vadim_makarenkov@mail.ru>
2020-07-21 18:49:42 +0300

    Update utils.py hardcoded for phone

--- utils.py
+++ utils.py
@@ -4,8 +4,8 @@ from PIL import Image
 
 def load_image(filename, size=None, scale=None):
     img = Image.open(filename)
-    if size is not None:
-        img = img.resize((size, size), Image.ANTIALIAS)
+    if size None:
+        img = img.resize((1080, 1350), Image.ANTIALIAS)
     elif scale is not None:
         img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
     return img
