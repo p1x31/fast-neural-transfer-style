035499c052bf963d0144eb32d757fbed3a84eaac
Vadim <vadim_makarenkov@mail.ru>
2020-07-21 13:35:04 +0300

    working deploy

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use the official lightweight Python image.
+# https://hub.docker.com/_/python
+FROM python:3.7-slim
+
+# Copy local code to the container image.
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+COPY . ./
+
+
+RUN apt update
+RUN apt install -y python3-dev gcc
+RUN apt-get install libgtk2.0-dev -y
+# Install production dependencies.
+RUN pip install Flask gunicorn
+ADD requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+# Run the web service on container startup. Here we use the gunicorn
+# webserver, with one worker process and 8 threads.
+# For environments with multiple CPU cores, increase the number of workers
+# to be equal to the cores available.
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 app:app
--- app.py
+++ app.py
@@ -5,26 +5,22 @@ from transformer_net import TransformerNet
 from imutils.video import VideoStream
 from imutils import resize
 from flask import Flask, render_template, request
-from fastai.vision.image import open_image, image2np
 from PIL import Image as PILImage
 import numpy as np
 from flask import Flask, redirect, flash, request, url_for, send_file, jsonify
 import os
 from style_cam import style_frame, read_state_dict
 import json
-#import some shit
+# import some shit
 from flask_dropzone import Dropzone
 import re
 import utils
 import random
 
-import onnx
-import onnx_caffe2.backend
-
 ALLOWED_EXTENSIONS = set(['bmp', 'png', 'jpg', 'jpeg', 'gif'])
-app=Flask(__name__)
+app = Flask(__name__)
 app.secret_key = "secret key"
-path=os.path.abspath(os.curdir)
+path = os.path.abspath(os.curdir)
 
 
 if app.config["DEBUG"]:
@@ -43,16 +39,6 @@ else:
     device = torch.device('cpu')
     print('CUDA device not available.')
     print('<<< Using CPU >>>')
-
-# weights = []
-# model_list={}
-# for idx,file in enumerate(os.listdir('models')):
-#     wts=read_state_dict(os.path.join(path,'models',file))
-#     weights.append(wts)
-#     model_list.update({file:idx})
-# model = TransformerNet()
-# model.to(device)
-
 
 dropzone = Dropzone(app)
 app.config.update(
@@ -74,15 +60,15 @@ def allowed_file(filename):
 @app.route('/')
 def upload():
     if request.method == 'POST':
-        input_img = request.files.getlist("file") #request.files['img'] 
-        #style = request.form['style']
+        input_img = request.files.getlist("file") # request.files['img'] 
+        # style = request.form['style']
         return send_file(input_img, mimetype='image/jpeg') and redirect(url_for('style'))
     return render_template('upload.html')
 
 
 @app.route('/predict', methods=['POST'])
 def style():
-    #some crappy checks
+    # some crappy checks
     print([i for i in request.form.keys()])
     response = {'Status': None,
                 'Message':None,
@@ -92,43 +78,17 @@ def style():
         response['Status'] = 'Failed'
         response['Message'] = 'No file attached to the request.'
         return json.dumps(response)
-    #request file and style
-    file=request.files['file']
+    # request file and style
+    file = request.files['file']
     content_image = utils.load_image(file)
     style = request.form['style']
     model_get = style
-    #some crappy checks
+    # some crappy checks
     if file.filename == '':
         response['Status'] = 'Failed'
         response['Message'] = 'No file attached to the request.'
         return json.dumps(response)
     if file and allowed_file(file.filename):
-        #save orig file
-        #TODO
-        #filename = file.filename
-        #img_path=os.path.join(path,'static/input',filename)
-        #
-        #file.save(img_path)
-        #img = Image.open(BytesIO(file.read())).convert('RGB')
-        #TODO 
-        # content_image.save(img_path)
-        #
-        #print(img_path)
-        #img=cv2.imread(img_path)
-        #print some shit
-        #print([k for k in request.form.keys()])
-        #style = request.form['style']
-        #print('\n\n',style,'\n\n')
-        #not working loop
-        # if style+'.pth' not in model_list.keys():
-        #     idx_model = 0
-        # else:
-        #     idx_model = model_list[style+'.pth']
-        # model.load_state_dict(weights[idx_model])
-        # #output
-        # output = style_frame(img,model,device)
-        # cv2.imwrite('result.jpg',output[:,:,::-1])
-        # return send_file('result.jpg')
         content_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Lambda(lambda x: x.mul(255))
@@ -160,94 +120,5 @@ def style():
     return render_template("index.html", get_image=get_image)
 
 
-# def encode(img):
-#     img = (image2np(img.data) * 255).astype('uint8')
-#     pil_img = PILImage.fromarray(img)
-#     buff = BytesIO()
-#     pil_img.save(buff, format="JPEG")
-#     return base64.b64encode(buff.getvalue()).decode("utf-8")
-
-# def _predict_single(fp):
-#   img = open_image(fp)
-#   pred = img.predict(_learn)
-#   idx = pred.argmax()
-#   label = _learn.data.classes[idx]
-#   img_data = encode(img)
-#   return { 'label': label, 'name': fp.filename, 'image': img_data }
-
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#   global _learn
-#   files = request.files.getlist("files")
-#   predictions = map(_predict_single, files)
-#   return render_template('predict.html', predictions=predictions)
-
-#Refactor this later please
-
-@app.route("/uploader", methods=['POST'])
-def upload_file():
-    # Check valid image and compress
-    try:
-        img = Image.open(BytesIO(request.files['imagefile'].read())).convert('RGB')
-        img = ImageOps.fit(img, (400, 400), Image.ANTIALIAS)
-    except:
-        error_msg = "Please choose an image file"
-        return render_template('index.html', **locals())
-    #TODO
-    #out_img = rundeeplearning(np.array(img), "Model/la_muse.ckpt")
-    img_io = BytesIO()
-    out_img.save(img_io, 'PNG')
-    # Display re-sized picture
-    png_output = base64.b64encode(img_io.getvalue())
-    processed_file = urllib.parse.quote(png_output)
-    return render_template('index.html', **locals())
-@app.route("/api/styles", methods=['GET'])
-def return_styles():
-    return jsonify(models_dict)
-
-@app.route("/api/uploader/<style>", methods=['POST'])
-def api_upload_file(style):
-    img = Image.open(BytesIO(request.files['imagefile'].read())).convert('RGB')
-    if min(img.size) > 400:
-        img = ImageOps.fit(img, (400, 400), Image.ANTIALIAS)
-    in_img = np.array(img)
-    model_file = models_dict[style]
-    out_img =  rundeeplearning(in_img, model_file)
-    return send_pil(out_img)
-
-### Helpers utility functions
-def send_pil(pil_img):
-    img_io = BytesIO()
-    pil_img.save(img_io, 'JPEG', quality=90)
-    img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
-
-def scale_img(style_path, style_scale):
-    scale = float(style_scale)
-    o0, o1, o2 = scipy.misc.imread(style_path, mode='RGB').shape
-    scale = float(style_scale)
-    new_shape = (int(o0 * scale), int(o1 * scale), o2)
-    style_target = _get_img(style_path, img_size=new_shape)
-    return style_target
-
-# def get_img(src, img_size=False):
-#    img = src  # Already pill image
-#    if not (len(img.shape) == 3 and img.shape[2] == 3):
-#        img = np.dstack((img,img,img))
-#    if img_size != False:
-#        img = scipy.misc.imresize(img, img_size)
-#    return img
-
-
-
-@app.route('/list',methods=['GET'])
-def list_of_models():
-    mlist = [os.path.splitext(m)[0] for m in model_list]
-    return jsonify({'list':mlist})
-
-if __name__=='__main__':
-    print('Server starting')
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port,debug=True)
-
+if __name__== '__main__':
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
